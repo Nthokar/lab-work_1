b3ff96c0c113cd4741416ec61e0bc82869c1a4cf
Nthokar <zhvl03@gmail.com>
2022-11-25 17:53:57 +0500

    commit

--- exercise.py
+++ exercise.py
@@ -1,45 +1,17 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
-x = [3,21,22,34,54,34,55,67,89,99]
-x = np.array(x)
-y = [2,22,24,65,79,82,55,130,150,199]
-y = np.array(y)
+era = list(range(8))
+total_error = {
+    "OR":[2, 2, 1, 0],
+    "AND":[2, 2, 3, 2, 1, 0],
+    "NAND":[2, 2, 3, 2, 1, 0]
+    }
 
-plt.scatter(x,y)
+ig, ax = plt.subplots()
 
-def model(a, b, x):
-	return a*x + b
+ax.bar(range(len(total_error["NAND"])), total_error["NAND"])
 
-def loss_function(a, b, x, y):
-	num = len(x)
-	prediction=model(a, b, x)
-	return (0.5/num) * (np.square(prediction-y)).sum()
+plt.show()
 
-def optimize(a, b, x, y):
-	num = len(x)
-	prediction = model(a, b, x)
-	da = (1.0/num) * ((prediction -y)*x).sum()
-	db = (1.0/num) * ((prediction -y).sum())
-	a = a - Lr*da
-	b = b - Lr*db
-	return a, b
-
-def iterate(a, b, x, y, times):
-	for i in range(times):
-		a,b = optimize(a,b,x,y)
-	return a,b
-
-a = np.random.rand(1)
-print(a)
-b = np.random.rand(1)
-print(b)
-Lr = 0.000001
-
-for i in range(0):
-	a,b = iterate(a,b,x,y,2)
-	prediction = model(a,b,x)
-	loss = loss_function(a, b, x, y)
-	print(a, b, loss)
-	plt.scatter(x, y)
-	plt.plot(x, prediction)
+print(era)
